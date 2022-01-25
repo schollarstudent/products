7197c4c374517834fcbdece3a022edb078e354c5
fmul <fmul@fresnou.org>
2022-01-25 08:54:31 -0800

    initial commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 const {Product} = require('../models')
 module.exports.renderProfile = async function (req,res){
-    const product = await product.findByPk(
+    const product = await Product.findByPk(
         req.params.id
     );
     res.render('products/profile', {product});
