7aa35aec3f4c60b66384f2be0e75a48e64eea8af
fmul <fmul@fresnou.org>
2022-02-08 08:55:43 -0800

    initial commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,6 @@
 const {Product} = require('../models')
+const categories = ['appliances','pets','home goods','other'];
+
 module.exports.renderProfile = async function (req,res){
     const product = await Product.findByPk(
         req.params.id
@@ -6,3 +8,22 @@ module.exports.renderProfile = async function (req,res){
     res.render('products/profile', {product});
 }
 
+module.exports.renderEditForm = async function (req,res) {
+    const product = await Product.findByPk(
+        req.params.id
+    );
+    res.render('products/edit',{product,categories});
+}
+
+module.exports.updateProduct= async function (req,res) {
+    await Product.update( {
+        product_name:req.body.product_name,
+        category: req.body.category,
+        description:req.body.description
+    }, {
+        where:{
+            id:req.params.id
+        }
+    });
+    res.redirect(`/products/profile/${req.params.id}`);
+}
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/products/profile/:id',productController.renderProfile);
+router.get('/products/edit/:id',productController.renderEditForm);
+router.post('/products/edit/:id',productController.updateProduct);
 module.exports = router;
